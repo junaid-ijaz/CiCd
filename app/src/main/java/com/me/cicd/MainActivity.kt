06c4e3d78aecb1f7dcc68a57ed91b2c93147a348
junaidijaz <junaid.ijaz@rockvillegroup.com>
2023-03-08 13:05:03 +0500

    firebase-distribution

--- app/src/main/java/com/me/cicd/MainActivity.kt
+++ app/src/main/java/com/me/cicd/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CicdTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(modifier = Modifier.fillMaxSize( ), color = MaterialTheme.colors.background) {
                     Greeting("Android")
                 }
             }
